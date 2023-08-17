4df5c49831e1c0eb3c958bdc7febd285e46917c2
cugljd <cugljd@qq.com>
2023-08-17 11:07:48 +0800

    Add Cesium

--- craco.config.js
+++ craco.config.js
@@ -1,1 +1,39 @@
-module.exports = {};
+const webpack = require("webpack");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  webpack: {
+    configure: {
+      // https://github.com/CesiumGS/cesium-webpack-example/blob/main/webpack.config.js
+      resolve: {
+        fallback: { https: false, zlib: false, http: false, url: false },
+      },
+    },
+    plugins: {
+      add: [
+        new CopyPlugin({
+          patterns: [
+            // Copy Cesium Assets, Widgets, and Workers to a static directory
+            {
+              from: "node_modules/cesium/Build/Cesium/Workers",
+              to: "cesium/Workers",
+            },
+            { from: "node_modules/cesium/Source/Assets", to: "cesium/Assets" },
+            {
+              from: "node_modules/cesium/Source/Widgets",
+              to: "cesium/Widgets",
+            },
+            {
+              from: "node_modules/cesium/Source/ThirdParty",
+              to: "cesium/ThirdParty",
+            },
+          ],
+        }),
+        new webpack.DefinePlugin({
+          // Define relative base path in cesium for loading assets
+          CESIUM_BASE_URL: JSON.stringify("/cesium"),
+        }),
+      ],
+    },
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Ion,
+  Viewer,
+  createWorldTerrainAsync,
+  //createOsmBuildingsAsync,
+  Cartesian3,
+  Math,
+} from "cesium";
+import "cesium/Build/Cesium/Widgets/widgets.css";
+import "./App.css";
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  useEffect(() => {
+    let ignore = false;
+
+    const initialize = async () => {
+      // Your access token can be found at: https://cesium.com/ion/tokens.
+      // This is the default access token
+      Ion.defaultAccessToken =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYWE1OWUxNy1mMWZiLTQzYjYtYTQ0OS1kMWFjYmFkNjc5YzciLCJpZCI6NTc3MzMsImlhdCI6MTYyNzg0NTE4Mn0.XcKpgANiY19MC4bdFUXMVEBToBmqS8kuYpUlxJHYZxk";
+
+      // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+      const viewer = new Viewer("cesiumContainer", {
+        terrainProvider: await createWorldTerrainAsync(),
+      });
+
+      // Add Cesium OSM Buildings, a global 3D buildings layer.
+      //viewer.scene.primitives.add(createOsmBuildingsAsync());
+
+      // Fly the camera to San Francisco at the given longitude, latitude, and height.
+      viewer.camera.flyTo({
+        destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+        orientation: {
+          heading: Math.toRadians(0.0),
+          pitch: Math.toRadians(-15.0),
+        },
+      });
+
+      if (ignore) {
+        viewer.destroy();
+      }
+    };
+
+    initialize();
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  return <div className="container" id="cesiumContainer"></div>;
+};
 
 export default App;
